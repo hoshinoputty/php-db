fdb0e122291bea6157feca15e3bd84a3d23d0f2c
Yohei-Hoshino <personl.yohei@gmail.com>
2024-11-11 08:41:34 +0900

    second commit

---
+++ select.php
@@ -0,0 +1,42 @@
+<?php
+  $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
+  $user = 'root';
+  $password = 'root';
+
+  try {
+    $pdo = new PDO($dsn, $user, $password);
+
+    $sql = 'SELECT id, name FROM users';
+
+    $stmt = $pdo->query($sql);
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  } catch (PDOException $e) {
+  exit($e->getMessage());
+  }
+?>
+
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP+DB</title>
+ </head>
+ 
+ <body>
+     <table>
+        <tr>
+          <th>ID</th>
+          <th>氏名</th>
+        </tr>
+        <?php
+        foreach($results as $result) {
+          echo "<tr><td>{$result['id']}</td><td>{$result['name']}</td></tr>";
+        }
+        ?>
+     </table>
+ </body>
+ 
+ </html>
